5f5a89e8d0abcb0fa64ef1150418c97b990bd721
cazcullimore <cazcullimore@gmail.com>
2023-09-25 21:30:16 -0600

    added a basic incentive to do different things, but commented it out for now because we need to fix the grads: it can't learn on mnist

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -8,7 +8,7 @@ import jax
 import jax.numpy as jnp
 # Special transform functions (we'll understand what these are very soon!)
 from jax import grad
-from jax import random
+import random
 import numpy as np
 
 
@@ -18,7 +18,7 @@ import numpy as np
 
 def sigmoid(x):
     exped = jnp.exp(-x)
-    exped = exped.at[exped > 100000000].set(100000000)
+    # exped = exped.at[exped > 100000000].set(100000000)
     return 2 * (1 / (1 +exped)) - 1
 
 
@@ -60,7 +60,7 @@ class NeuralNetwork():
         self.weights = []  ## list of matrices that is the connections between the nodes
         for i in range(len(layerSizes)):
             try:
-                self.weights.append(random.normal(jax.random.PRNGKey(100), shape=(layerSizes[i], layerSizes[i + 1])))
+                self.weights.append(jax.random.normal(jax.random.PRNGKey(100), shape=(layerSizes[i], layerSizes[i + 1])))
             except IndexError:
                 pass
 
@@ -70,10 +70,8 @@ class NeuralNetwork():
         self.activaction = activaction
         self.derivativeOfActivation = derivativeOfActivation
         self.layerSizes = layerSizes
-        self.avgPastWeights = []  # rolling avg of past weights (give slight gradient away from their)
+        self.latestGradSteps = [self.weights, ] * 10  # last 10 weight grads (just init so don't get shape error)
         self.ouputFunction = activaction
-        self.x = []
-        self.y = []
 
     def forwardPass(self, x, weights=[], lazyness=0):
         """ x : is input, has shape of
@@ -96,27 +94,65 @@ class NeuralNetwork():
         # print(deltas)
         return output
 
-    def getError(self, weights, lazyness=0):
-        yhat = self.forwardPass(self.x, weights)
-        error = jnp.mean((self.ouputFunction(yhat) - self.y) ** 2)
+    def getError(self,x,y, weights, lazyness=0):
+        yhat = self.forwardPass(x, weights)
+        pastWeightGrads1 = self.latestGradSteps[-2]
+        pastWeightGrads2 = self.latestGradSteps[-1]
+        diffSum = 0
+        for layerIndex in range(len(pastWeightGrads1)):
+            diffSum += jnp.linalg.norm(
+                pastWeightGrads1[layerIndex] / jnp.linalg.norm(pastWeightGrads1[layerIndex]) - pastWeightGrads2[
+                    layerIndex] / jnp.linalg.norm(pastWeightGrads2[layerIndex]))
+        diffSum /= layerIndex + 1
+
+        error = jnp.mean((self.ouputFunction(yhat) - y) ** 2)
+        if not jnp.isnan(diffSum) and diffSum != 0:
+            error /= diffSum
         return error
 
-    def train(self, x, y, numIter=2, lazyness=0):
-        self.x = x  # probably not the best strat
-        self.y = y
+    def train(self, x, y, batchSize = 50000000, numIter=2, lazyness=0):
+        gradFunc = grad(self.getError, argnums=2)
         for i in range(numIter):
-            yhat = self.forwardPass(x, lazyness=lazyness)
+            printIter = i % 100 == 0
+            # if batchSize != -1
+            numBatches = (x.shape[0]/batchSize)
+            randBatchNumber = random.randint(0,round(numBatches))
+            batchX = x[randBatchNumber*batchSize:(randBatchNumber + 1)*batchSize]  # probably not the best strat
+            batchY = y[randBatchNumber*batchSize:(randBatchNumber + 1)*batchSize]
 
             ## update
-            weightGrads = grad(self.getError)(self.weights)
+            weightGrads = gradFunc(batchX,batchY,self.weights)
             # print(jax.make_jaxpr(grad(self.getError)))
+
+            weightGradsTimesLearningRate = []
+
+
             for index in range(len(self.weights)):
+                # momentumGrad = jnp.zeros_like(self.weights[index])
+                # for pastWeightGrad in self.avgPastWeights:
+                #     momentumGrad += pastWeightGrad[index]
+                # momentumGrad /= len(self.avgPastWeights)
                 # weightGrads[index] = weightGrads[index].at[weightGrads[index] > 100000000].set(0)
-                weightGrads[index] = weightGrads[index].at[jnp.isnan(weightGrads[index])].set(0)
-                self.weights[index] -= weightGrads[index] * self.learningRate
+                # weightGrads[index] = weightGrads[index].at[jnp.isnan(weightGrads[index])].set(0)
+                self.weights[index] -= (weightGrads[index] * self.learningRate) #+ momentumGrad
+                if printIter:
+                    weightGradsTimesLearningRate.append(weightGrads[index] * self.learningRate)
+
 
             # ranNum = random.randint(0,yhat.shape[1]-1)
-            if i % 10 == 0:
+            if printIter:
+                self.latestGradSteps.pop(0)
+                self.latestGradSteps.append(weightGradsTimesLearningRate)
+                for pastWeightGradIndex in range(len(self.latestGradSteps) - 1):
+                    pastWeightGrads1 = self.latestGradSteps[pastWeightGradIndex]
+                    pastWeightGrads2 = weightGradsTimesLearningRate
+                    diffSum = 0
+                    for layerIndex in range(len(pastWeightGrads1)):
+                        diffSum += jnp.linalg.norm(pastWeightGrads1[layerIndex]/jnp.linalg.norm(pastWeightGrads1[layerIndex]) - pastWeightGrads2[layerIndex]/jnp.linalg.norm(pastWeightGrads2[layerIndex]))
+                    diffSum /= layerIndex + 1
+                    print("gradient distances", len(self.latestGradSteps) - pastWeightGradIndex - 1, "iterations away:", diffSum)
+                print("----------")
+                yhat = self.forwardPass(x, lazyness=lazyness)
                 error = jnp.sum((self.ouputFunction(yhat) - y) ** 2)
                 print("iteration:", i, "error", error)
                 if jnp.isnan(error):
--- main.py
+++ main.py
@@ -1,17 +1,13 @@
+NUM_SAMPLES_TO_READ = 240000  # everything
+SLEEP_TIME = 0.001
+MAKE_ONE_HOT_ENCODING = True
 from NeuralNetwork import NeuralNetwork
 import numpy as np
 import sklearn.datasets as datasets
-import cProfile
-import re
-# import matplotlib.pyplot as plt
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-# from NeuralNetwork import NeuralNetwork
-import numpy as np
-import sklearn.datasets as datasets
 from time import time
+import tensorflow as tf
+import jax.numpy as jnp
+tf.config.set_visible_devices([], device_type='GPU')
 # JAX_DEBUG_NANS=True
 # from jax import config
 # config.update("jax_debug_nans", True)
@@ -21,34 +17,70 @@ import re
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-# prof = cProfile.Profile()
-# prof.enable()
+prof = cProfile.Profile()
+prof.enable()
 # Press the green button in the gutter to run the script.
+def importRNAData(fileName):
+    firstLine = True
+    with open(fileName) as dataFile:
+        reactivityFirstIndex = -1
+        reactivityLastIndex = -1
+        indexesOfReactivity = []
+        for line in dataFile:
+            cols = line.split(',')
+            if firstLine:
+                for index in range(len(cols)): # there's also "experiment_type,dataset_name,reads,signal_to_noise,SN_filter" that might be useful
+                    if cols[index] == 'sequence_id':
+                        indexOfSeqId = index
+                    elif cols[index] == 'sequence':
+                        indexOfSeqNucs = index
+                    elif "reactivity_" in cols[index] and not ("error" in cols[index].split("_")):
+                        if reactivityFirstIndex == -1:
+                            reactivityFirstIndex = index
+                        reactivityLastIndex = index
+                firstLine = False
+                # continue
+            seqId = cols[indexOfSeqId]
+            seqNucs = cols[indexOfSeqNucs]
+            reactivity = cols[reactivityFirstIndex:reactivityLastIndex]
+            print(reactivity)
+
 if __name__ == '__main__':
+
+    # importRNAData("train_data.csv") # TODO: finish later
+
     t1 = time()
     ## test neural network
-    hiddenLayerSizes = [30,20,10]
+    hiddenLayerSizes = [512, 256, 256]
 
     ## test weight sizes
     # print("size", len(nn.weights))
 
     # dataset = datasets.load_iris()
-    dataset = datasets.load_digits()
-    dataset.keys()
+    # dataset = datasets.load_digits()
+    # xTrain = dataset["data"]
+    # yTrain = dataset["target"]
+    train, test = tf.keras.datasets.mnist.load_data()
+    xTrain = jnp.array(train[0])
+    xTrain = jnp.reshape(xTrain, (xTrain.shape[0], xTrain.shape[1]*xTrain.shape[2]))
+    yTrain = jnp.array(train[1])
+    # dataset = tf.data.
+    # print(dataset.keys())
     # data = np.random.uniform(size=[1,10])
     # nn.forwardPass(digits[0])
 
-    numOutputGroups = max(dataset["target"]) + 1
-    oneHotTarget = np.zeros(shape=(dataset["target"].shape[0], numOutputGroups)) # magic number
+    numOutputGroups = max(yTrain) + 1
+    oneHotTarget = np.zeros(shape=(yTrain.shape[0], numOutputGroups)) # magic number
     hiddenLayerSizes.append(numOutputGroups) # add output layers
-    hiddenLayerSizes = [dataset["data"].shape[1]] + hiddenLayerSizes
-    i = -1
-    for answer in dataset["target"]:
-        i += 1
-        oneHotTarget[i, answer] = 1
+    hiddenLayerSizes = [xTrain.shape[1]] + hiddenLayerSizes
 
-    NUM_SAMPLES_TO_READ = 2400 # everything
-    SLEEP_TIME = 0.0
+    if MAKE_ONE_HOT_ENCODING:
+        i = -1
+        for answer in yTrain:
+            i += 1
+            oneHotTarget[i, answer] = 1
+        yTrain = oneHotTarget
+
 
     nn = NeuralNetwork(hiddenLayerSizes)
     assert len(nn.weights) == len(hiddenLayerSizes) - 1
@@ -56,15 +88,15 @@ if __name__ == '__main__':
         assert nn.weights[i].shape[0] == hiddenLayerSizes[i] ## is input size right
         assert nn.weights[i].shape[1] == hiddenLayerSizes[i + 1] ## is output size right
 
-
     # nn.learningRate = 0.5
     # nn.train(dataset["data"][:NUM_SAMPLES_TO_READ], oneHotTarget[:NUM_SAMPLES_TO_READ], numIter=5, lazyness=SLEEP_TIME)
-    nn.learningRate = 0.1
-    nn.train(dataset["data"][:NUM_SAMPLES_TO_READ], oneHotTarget[:NUM_SAMPLES_TO_READ], numIter=1000, lazyness=SLEEP_TIME)
     nn.learningRate = 0.01
-    nn.train(dataset["data"][:NUM_SAMPLES_TO_READ], oneHotTarget[:NUM_SAMPLES_TO_READ], numIter=1000, lazyness=SLEEP_TIME)
-    # nn.learningRate = 0.001
-    # nn.train(dataset["data"][:NUM_SAMPLES_TO_READ], oneHotTarget[:NUM_SAMPLES_TO_READ], numIter=20, lazyness=SLEEP_TIME)
+    nn.train(xTrain[:NUM_SAMPLES_TO_READ], yTrain[:NUM_SAMPLES_TO_READ], numIter=100, lazyness=SLEEP_TIME)
+    nn.learningRate = 0.000001
+    nn.train(xTrain[:NUM_SAMPLES_TO_READ], yTrain[:NUM_SAMPLES_TO_READ], numIter=100000, lazyness=SLEEP_TIME)
+    nn.learningRate = 0.001
+    print('increase')
+    nn.train(xTrain[:NUM_SAMPLES_TO_READ], yTrain[:NUM_SAMPLES_TO_READ], numIter=1000, lazyness=SLEEP_TIME)
     # x = np.zeros(shape=(2,4,5,6,7,))
     # # print(np.matmul(x, x.transpose()).shape)
     # # for i in range(5):
@@ -72,9 +104,9 @@ if __name__ == '__main__':
     # print(x.shape)
     # x = np.transpose(x,axes=(4,3,2,1,0))
     # print(x.shape)
-    print(time()-t1)
-# prof.disable()
-# prof.create_stats()
-# prof.print_stats()
+    print("time",time()-t1)
+prof.disable()
+prof.create_stats()
+prof.print_stats()
 
 
